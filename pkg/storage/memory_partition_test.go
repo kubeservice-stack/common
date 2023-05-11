a5cecc7829c710db596945a26375143b1e64a030
dongjiang1989 <dongjiang1989@126.com>
2023-05-11 22:11:25 +0800

    dongjiang, unittest case && remove 3d libray

--- pkg/storage/memory_partition_test.go
+++ pkg/storage/memory_partition_test.go
@@ -171,25 +171,6 @@ func Test_memoryPartition_SelectDataPoints(t *testing.T) {
 	}
 }
 
-type fakeEncoder struct {
-	encodePointFunc func(*DataPoint) error
-	flushFunc       func() error
-}
-
-func (f *fakeEncoder) encodePoint(p *DataPoint) error {
-	if f.encodePointFunc == nil {
-		return nil
-	}
-	return f.encodePointFunc(p)
-}
-
-func (f *fakeEncoder) flush() error {
-	if f.flushFunc == nil {
-		return nil
-	}
-	return f.flushFunc()
-}
-
 func Test_toUnix(t *testing.T) {
 	tests := []struct {
 		name      string
