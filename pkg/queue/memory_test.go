6ce5648618c4dd6641c923062bab6dc49881e9c7
dongjiang1989 <dongjiang1989@126.com>
2023-07-06 11:31:09 +0800

    dongjiang, add unittest
    Signed-off-by: dongjiang1989 <dongjiang1989@126.com>

--- pkg/queue/memory_test.go
+++ pkg/queue/memory_test.go
@@ -23,9 +23,12 @@ import (
 	"sync"
 	"sync/atomic"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMemoryQueue(t *testing.T) {
+	assert := assert.New(t)
 	var wg sync.WaitGroup
 	var id int32
 
@@ -35,7 +38,7 @@ func TestMemoryQueue(t *testing.T) {
 	wg.Add(producter)
 
 	q := NewUnLockQueue(1024 * 1024)
-
+	assert.Equal(q.Length(), int64(0))
 	for i := 0; i < producter; i++ {
 		go func(g int) {
 			defer wg.Done()
@@ -68,4 +71,39 @@ func TestMemoryQueue(t *testing.T) {
 	} else {
 		log.Println("Len:", q)
 	}
+}
+
+func TestMaxlen(t *testing.T) {
+	assert := assert.New(t)
+	q := new(UnLockQueue)
+	assert.Equal(q.Maxlen(), uint64(0))
+	assert.Equal(q.IsEmpty(), true)
+	q.maxlen = 10
+	assert.Equal(q.Maxlen(), uint64(10))
+}
+
+func TestLength(t *testing.T) {
+	assert := assert.New(t)
+	q := new(UnLockQueue)
+	assert.Equal(q.Maxlen(), uint64(0))
+	assert.Equal(q.IsEmpty(), true)
+	q.maxlen = minLen(2)
+	q.capM = q.maxlen - 1
+	q.mp = make([]Item, q.maxlen)
+
+	ok := q.Push("a")
+	assert.True(ok)
+	ok = q.Push("b")
+	assert.False(ok)
+	ok = q.Push("c")
+	assert.False(ok)
+
+	assert.Equal(q.Length(), int64(1))
+
+	bb, ok := q.Pop()
+	assert.True(ok)
+	assert.Equal(bb, "a")
+	bb, ok = q.Pop()
+	assert.False(ok)
+	assert.Equal(bb, nil)
 }
